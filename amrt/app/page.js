b05d0a424fb8e8c7dd1367092bd2067db558f57c
Ammaar B <a.bedasso12@gmail.com>
2025-06-24 19:44:07 -0600

    sussy baka

--- amrt/app/page.js
+++ amrt/app/page.js
@@ -1,8 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
       <h1>AMRT App</h1>
-      <p>Welcome to the AMRT application!</p>
+      <p><Link href="/join" prefetch={false}>Welcome to the AMRT application! </Link></p>
+      <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      />
     </div>
   );
 }
+
