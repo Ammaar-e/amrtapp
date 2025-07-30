2bc6cb82969ff29905e82f088e582142d26ac040
Ammaar-e <62914394+Ammaar-e@users.noreply.github.com>
2025-07-29 21:04:20 -0600

    Add files via upload

---
+++ f250shelby/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version('cerulean')
+game('gta5')
+lua54 'on'
+
+files({
+    'data/carcols.meta',
+    'data/carvariations.meta',
+    'data/contentunlocks.meta',
+    'data/handling.meta',
+    'data/vehiclelayouts.meta',
+    'data/vehicles.meta',
+    'data/dlctext.meta'
+})
+
+-- Vehicle stuff
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'contentunlocks.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_SHOP_DLC_FILE' 'vehicle_shop_dlc.meta'
+data_file 'VEHICLE_WEAPON_SETTINGS_FILE' 'vehicleweaponsettings.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_SHOP_DLC_FILE' 'vehicle_shop_dlc.meta'
+data_file 'VEHICLE_WEAPON_SETTINGS_FILE' 'vehicleweaponsettings.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_SHOP_DLC_FILE' 'vehicle_shop_dlc.meta'
+data_file 'DLC_TEXT_FILE' 'dlctext.meta'
